a2ece6c450e3a5d739c08a61689d3dfcc41324b2
Ricardo <ricardo.machado.nwi@gmail.com>
2021-07-09 23:55:42 -0300

    Fixed Logo  is Missing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 import '../styles/header.scss'
 
 export function Header() {
+  const logo = 'https://raw.githubusercontent.com/DrZanuff/ignite-reactjs-desafio-01/c57a4b9a598c09f09106b82359ed3b45a5106014/public/logo.svg'
   return (
     <header className="header">
       <div>
-        <img src="/logo.svg" alt="to.do"/>
+        <img src={logo} alt="to.do"/>
       </div>
     </header>
   )
